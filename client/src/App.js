f2549fcd01aafa2549f62f5fb94491bfd004e4c9
Andrew <ATHur1104@gmail.com>
2023-09-07 21:31:51 -0500

    updated test to have working game api call

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import Test from './pages/Test';
+import Test from './pages/ApiTest';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
