8e1ca8fc07b2bb53dbf6db5e1d1bcdf3de43bcd3
rylawss <128633609+rylawss@users.noreply.github.com>
2023-09-07 20:43:01 -0500

    added NotesList and NoteForm

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,33 @@
 import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import { useQuery } from "@apollo/client";
+
+import NoteForm from "../components/NoteForm";
+import NoteList from "../components/NoteList";
+
+import { QUERY_THOUGHTS } from "../utils/queries";
 
 const Home = () => {
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const notes = data?.notes || [];
+
   return (
-    <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
-    </div>
+    <main>
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: "1px dotted #1a1a1a" }}
+        >
+          <NoteList />
+        </div>
+        <div className="col-12 col-md-8 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <NoteForm notes={notes} title="Some Feed for Thought(s)..." />
+          )}
+        </div>
+      </div>
+    </main>
   );
 };
 
