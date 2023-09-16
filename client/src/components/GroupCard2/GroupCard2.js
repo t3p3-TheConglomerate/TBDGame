8bafe87110ad9da325e6b3bc1d6275c7d7718eca
unknown <stam296@gmail.com>
2023-09-16 02:43:53 -0500

    game data works on homepage and group page

---
+++ client/src/components/GroupCard2/GroupCard2.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { QUERY_SINGLE_GROUP, QUERY_SINGLE_USER } from "../../utils/queries";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import spinner from "../../assets/spinner.gif";
+
+const GroupCard2 = ({group}) => {
+
+   console.log("group",group);
+   const { loading, data } = useQuery(QUERY_SINGLE_GROUP, {
+        variables: { id: group },
+    });
+
+    console.log("data",data);
+
+    return (
+        <Col key={data?.group?.groupName} md="4">
+        <Card border="dark">
+            {data?.group?.gameImage ? (
+            <Card.Img
+                src={data?.group?.gameImage}
+                alt={`The cover for ${data?.group?.groupName}`}
+                variant="top"
+            />
+            ) : null}
+            <Card.Body key={data?.group?.groupId}>
+            <Card.Title>{data?.group?.groupName}</Card.Title>
+            <p className="small">Group: {data?.group?.groupName}</p>
+            <Card.Text>{data?.group?.gameDescription}</Card.Text>
+            <Button
+                className="btn-block btn-danger"
+            >
+                <Link to={`/group/${group}`}>View Group</Link>
+            </Button>
+            </Card.Body>
+        </Card>
+    </Col>
+    );
+};
+
+export default GroupCard2;
--- client/src/components/GroupList/index.js
+++ client/src/components/GroupList/index.js
@@ -6,6 +6,7 @@ import spinner from "../../assets/spinner.gif";
 import { QUERY_GROUP } from "../../utils/queries";
 import { GET_ME } from "../../utils/queries";
 import { Link } from "react-router-dom";
+import GroupCard2 from "../GroupCard2/GroupCard2";
 
 // query group then match id with url id and get name
 
@@ -13,40 +14,43 @@ import { Link } from "react-router-dom";
 function GroupList() {
   const { loading, data } = useQuery(GET_ME);
   const userData = data?.me || data?.user || {};
-  // console.log(data);
-
+  console.log("me data",data);
+  console.log("user data",userData);
+  console.log(userData?.groups?.map((group) => {console.log(group._id)}));
 
 
   return (
     <>
       <div className="Row">
-        {userData &&
-          userData?.groups &&
-          userData?.groups?.map((group) => {
-            return (
-              <Col key={group.groupName} md="4">
-                <Card border="dark">
-                  {group.gameImage ? (
-                    <Card.Img
-                      src={group.gameImage}
-                      alt={`The cover for ${group.groupName}`}
-                      variant="top"
-                    />
-                  ) : null}
-                  <Card.Body key={group.groupId}>
-                    <Card.Title>{group.groupName}</Card.Title>
-                    <p className="small">Group: {group.groupName}</p>
-                    <Card.Text>{group.gameDescription}</Card.Text>
-                    <Button
-                      className="btn-block btn-danger"
-                    >
-                      <Link to={`/group/${group._id}`}>View Group</Link>
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
+        {
+        // userData &&
+        //   userData?.groups &&
+          userData?.groups?.map((group) => (
+            <GroupCard2 group={group._id} key={group._id}/>
+            // return (
+            //   <Col key={group.groupName} md="4">
+            //     <Card border="dark">
+            //       {group.gameImage ? (
+            //         <Card.Img
+            //           src={group.gameImage}
+            //           alt={`The cover for ${group.groupName}`}
+            //           variant="top"
+            //         />
+            //       ) : null}
+            //       <Card.Body key={group.groupId}>
+            //         <Card.Title>{group.groupName}</Card.Title>
+            //         <p className="small">Group: {group.groupName}</p>
+            //         <Card.Text>{group.gameDescription}</Card.Text>
+            //         <Button
+            //           className="btn-block btn-danger"
+            //         >
+            //           <Link to={`/group/${group._id}`}>View Group</Link>
+            //         </Button>
+            //       </Card.Body>
+            //     </Card>
+            //   </Col>
+            // );
+          ))}
       </div>
     </>
   );
