6d17e9bd30828d67236bddc7f65f5335ba4ac056
Andrew <ATHur1104@gmail.com>
2023-09-17 11:32:54 -0500

    maybe a fix

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 app.use(
   "/api/search",
   createProxyMiddleware({
