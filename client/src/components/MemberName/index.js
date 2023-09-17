62bf1773482ee5bdbe719f440cc7bdcddd7d3053
Andrew <ATHur1104@gmail.com>
2023-09-17 13:57:01 -0500

    styling for groupmembers

--- client/src/components/MemberName/index.js
+++ client/src/components/MemberName/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { QUERY_SINGLE_USER } from "../../utils/queries";
+import "./index.css";
 
 const MemberList = (props) => {
 
@@ -12,7 +13,7 @@ const MemberList = (props) => {
   return (
     <div key={data?.user?._id} className="card mb-3">
       <div className="card-body bg-light p-2">
-        <p style={{ color: 'black' }}>{data?.user?.username}</p>
+        <p style={{ color: 'white' }}>{data?.user?.username}</p>
       </div>
     </div>
   );
