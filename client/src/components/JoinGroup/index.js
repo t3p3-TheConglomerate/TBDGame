d4a95df67952ec3e801ae2ada4ab4f24730936f0
unknown <stam296@gmail.com>
2023-09-17 20:52:36 -0500

    login button shows if invited to group but not logged in

--- client/src/components/JoinGroup/index.js
+++ client/src/components/JoinGroup/index.js
@@ -36,24 +36,28 @@ function JoinGroup({groupId, userId}) {
     const { data: meData } = useQuery(GET_ME);
 
     const isMember = !groupData?.group?.groupMembers?.includes(meData?.me?._id);
+    console.log(!isMember);
+    console.log(meData);
 
-    if (!isMember) {
-  return (
-<div className="joingroup p-4 my-2">
-  <p>Please Login to Join</p>
-  <Button onClick={groupJoin} className="mt-3 bg-primary">Join group</Button>
-  <Button 
-  onClick={home} 
-  className="mt-3 bg-danger">No thanks</Button>
-</div>  );
-} return (
-  <div className="joingroup p-4 my-2">
-    <p>Please Login to Join</p>
-    <Button onClick={groupJoin} className="mt-3 bg-primary">Join group</Button>
-    <Button 
-    onClick={home} 
-    className="mt-3 bg-danger">No thanks</Button>
-  </div>  )
+    if (!meData) {
+      return (
+        <div className="text-uppercase text-center p-4 my-2">
+          <p>Please Login to Join</p>
+          <a href="/login">
+            <Button className="mt-3 bg-primary">Login</Button>
+          </a>
+        </div>  
+      );
+    } 
+    else{
+    return (
+      <div className="joingroup p-4 my-2">
+        <Button onClick={groupJoin} className="mt-3 bg-primary">Join group</Button>
+        <Button 
+        onClick={home} 
+        className="mt-3 bg-danger">No thanks</Button>
+      </div>  )
+    }
 }
 
 export default JoinGroup;
