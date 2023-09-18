668cf3d260afdb82652eea1cccfcf57759091433
unknown <stam296@gmail.com>
2023-09-17 20:10:32 -0500

    copied url button state change

--- client/src/components/MemberList/index.js
+++ client/src/components/MemberList/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { QUERY_SINGLE_GROUP, QUERY_SINGLE_USER } from "../../utils/queries";
@@ -13,14 +13,24 @@ const MemberList = () => {
  
   const inviteLink = `${window.location.origin}/group/${group._id}`
 
-  const copyInvite = () => {
-    navigator.clipboard.writeText(inviteLink)
+  const initialState = "Click Here to Copy the Invite URL";
+  const [buttonText, setButtonText] = useState("Click Here to Copy the Invite URL");
+
+  useEffect(() => { 
+    if(buttonText !== initialState){
+      setTimeout(() => setButtonText(initialState), [5000])
+    }
+  }, [buttonText])
+
+  const copyInvite = (newText) => {
+    navigator.clipboard.writeText(inviteLink);
+    setButtonText(newText);
   }
 
   return (
     <div>
       <h3>Group Members</h3>
-      <p>Invite Others: <button onClick={copyInvite}>Click Here to Copy the Invite URL</button>
+      <p>Invite Others: <button onClick={() => copyInvite("URL Copied!")}>{buttonText}</button>
       </p>
         {group?.groupMembers?.map((member) => (
           <MemberName memberId={member} key={member._id}/>
